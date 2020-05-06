1f849acdd13111948d35a09951eb34f1b886a60b
Angelo Gargantini <angelo.gargantini@unibg.it>
2020-05-06 19:42:57 +0200

    frist draft

---
+++ src/MVM.cpp
@@ -0,0 +1,18 @@
+/*
+ * MVM.cpp
+ *
+ *  Created on: 6 mag 2020
+ *      Author: AngeloGargantini
+ */
+
+#include "MVM.h"
+
+MVM::MVM() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MVM::~MVM() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/MVM.h
@@ -0,0 +1,17 @@
+/*
+ * MVM.h
+ *
+ *  Created on: 6 mag 2020
+ *      Author: AngeloGargantini
+ */
+
+#ifndef MVM_H_
+#define MVM_H_
+
+class MVM {
+public:
+	MVM();
+	virtual ~MVM();
+};
+
+#endif /* MVM_H_ */
